b051427b87a9834c37d4ffeea4411cd3dafadf1a
supercmmetry <vishaals2000@gmail.com>
2020-10-19 18:49:26 +0530

    tests: Write test for get_all_files
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- tests/query.rs
+++ tests/query.rs
@@ -7,3 +7,10 @@ fn test_check_if_file_exists() {
 
     assert!(c.file_exists("/data.txt".to_string()).unwrap() == false);
 }
+
+#[test]
+fn test_get_all_files() {
+    let mut c = Connection::new("hzip://localhost:1729/?password=hybridzip&archive=test.hz");
+
+    assert!(c.all_files().unwrap() == vec![] as Vec<String>);
+}
