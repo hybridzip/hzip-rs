94211bb5c4df0b6d77e5282712256fa59fb6da04
supercmmetry <vishaals2000@gmail.com>
2020-10-20 19:12:08 +0530

    stream: Implement file-reader
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- src/control/stream.rs
+++ src/control/stream.rs
@@ -16,3 +16,15 @@ pub enum EncodeCtl {
     Piggyback = 0x5,
 }
 
+#[derive(FromPrimitive)]
+pub enum DecodeCtl {
+    Stream = 0x0,
+    Model = 0x1,
+    Archive = 0x2,
+    Source = 0x3,
+    Algorithm = 0x4,
+    Piggyback = 0x5,
+    ModelStream = 0x6,
+    BlobStream = 0x7,
+}
+
--- src/handlers/stream.rs
+++ src/handlers/stream.rs
@@ -2,10 +2,12 @@ use std::io::{Read, Write};
 use crate::connection::connection::Connection;
 use crate::handlers::session::SessionManager;
 use crate::utils::common::{write_ctl_word, write_ctl_string, read_ctl_word};
-use crate::control::api::ApiCtl;
-use crate::control::stream::{StreamCtl, EncodeCtl};
+use crate::control::api::{ApiCtl, CommonCtl};
+use crate::control::stream::{StreamCtl, EncodeCtl, DecodeCtl};
 use byteorder::{LittleEndian, ByteOrder};
 use num_derive::FromPrimitive;
+use anyhow::Error;
+
 
 #[derive(Debug, Clone, FromPrimitive, PartialEq)]
 pub enum Algorithm {
@@ -15,16 +17,22 @@ pub enum Algorithm {
 
 pub struct StreamConfig {
     pub filename: String,
-    pub algorithm: Algorithm,
+    pub algorithm: Option<Algorithm>,
     pub model: Option<String>,
 }
 
 pub trait Streamable {
     fn write_file<R: Read>(&mut self, reader: R, config: StreamConfig) -> Result<(), anyhow::Error>;
+
+    fn read_file<W: Write>(&mut self, writer: W, config: StreamConfig) -> Result<(), anyhow::Error>;
 }
 
 impl Streamable for Connection {
     fn write_file<R: Read>(&mut self, mut reader: R, config: StreamConfig) -> Result<(), anyhow::Error> {
+        if config.algorithm.is_none() {
+            return Err(anyhow!("Algorithm was not specified"));
+        }
+
         self.refresh_session()?;
 
         let stream = self.stream.as_mut().unwrap();
@@ -39,7 +47,7 @@ impl Streamable for Connection {
         write_ctl_string(stream, config.filename.clone())?;
 
         write_ctl_word(stream, EncodeCtl::Algorithm as u8)?;
-        write_ctl_word(stream, config.algorithm.clone() as u8)?;
+        write_ctl_word(stream, config.algorithm.unwrap().clone() as u8)?;
 
         if config.model.is_some() {
             write_ctl_word(stream, EncodeCtl::Model as u8)?;
@@ -58,6 +66,41 @@ impl Streamable for Connection {
         stream.write(&mut buf)?;
 
         read_ctl_word(stream)?;
+
+        Ok(())
+    }
+
+    fn read_file<W: Write>(&mut self, mut writer: W, config: StreamConfig) -> Result<(), Error> {
+        self.refresh_session()?;
+
+        let stream = self.stream.as_mut().unwrap();
+
+        write_ctl_word(stream, ApiCtl::Stream as u8)?;
+        write_ctl_word(stream, StreamCtl::Decode as u8)?;
+
+        write_ctl_word(stream, DecodeCtl::Archive as u8)?;
+        write_ctl_string(stream, self.archive.clone())?;
+
+        write_ctl_word(stream, DecodeCtl::Source as u8)?;
+        write_ctl_string(stream, config.filename.clone())?;
+
+        write_ctl_word(stream, DecodeCtl::Algorithm as u8)?;
+        write_ctl_word(stream, config.algorithm.unwrap().clone() as u8)?;
+
+        write_ctl_word(stream, DecodeCtl::Piggyback as u8)?;
+        write_ctl_word(stream, DecodeCtl::Stream as u8)?;
+
+        while read_ctl_word(stream)? == CommonCtl::PiggyBack {
+            let mut len_buf = [0 as u8; 8];
+            stream.read(&mut len_buf)?;
+
+            let len = LittleEndian::read_u64(&len_buf);
+
+            let mut buf: Vec<u8> = vec![0 as u8; len as usize];
+            stream.read(&mut buf)?;
+
+            writer.write(&buf)?;
+        }
 
         Ok(())
     }
