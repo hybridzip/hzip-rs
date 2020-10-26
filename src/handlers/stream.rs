a268b883d899ff161824dcd9da5ce0ffcd77687c
supercmmetry <vishaals2000@gmail.com>
2020-10-26 20:22:50 +0530

    stream: Implement read_model
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- src/handlers/stream.rs
+++ src/handlers/stream.rs
@@ -3,12 +3,14 @@ use std::io::{Read, Write};
 use anyhow::Error;
 use byteorder::{ByteOrder, LittleEndian};
 use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 
 use crate::connection::connection::Connection;
 use crate::control::api::ApiCtl;
-use crate::control::stream::{DecodeCtl, EncodeCtl, StreamCtl, ModelCtl};
+use crate::control::stream::{DecodeCtl, EncodeCtl, ModelCtl, StreamCtl};
 use crate::handlers::session::SessionManager;
-use crate::utils::common::{read_ctl_string, read_stream, write_ctl_string, write_ctl_word, write_stream};
+use crate::utils::common::{read_ctl_string, read_stream, write_ctl_string,
+                           write_ctl_word, write_stream};
 
 #[derive(Debug, Clone, FromPrimitive, PartialEq)]
 pub enum Algorithm {
@@ -28,6 +30,8 @@ pub trait Streamable {
     fn read_file<W: Write>(&mut self, writer: W, config: StreamConfig) -> Result<(), anyhow::Error>;
 
     fn write_model<R: Read>(&mut self, reader: R, config: StreamConfig) -> Result<(), anyhow::Error>;
+
+    fn read_model<W: Write>(&mut self, writer: W, config: StreamConfig) -> Result<Option<Algorithm>, anyhow::Error>;
 }
 
 impl Streamable for Connection {
@@ -149,5 +153,41 @@ impl Streamable for Connection {
         write_stream(stream, &buf)?;
 
         Ok(())
+    }
+
+    fn read_model<W: Write>(&mut self, mut writer: W, config: StreamConfig) -> Result<Option<Algorithm>, Error> {
+        if config.model.is_none() {
+            return Err(anyhow!("Model address was not specified"));
+        }
+
+        self.refresh_session()?;
+
+        let stream = self.stream.as_mut().unwrap();
+
+        write_ctl_word(stream, ApiCtl::Stream as u8)?;
+        write_ctl_word(stream, StreamCtl::ReadModel as u8)?;
+
+        write_ctl_word(stream, ModelCtl::Archive as u8)?;
+        write_ctl_string(stream, self.archive.clone())?;
+
+        write_ctl_word(stream, ModelCtl::Address as u8)?;
+        write_ctl_string(stream, config.model.unwrap())?;
+
+        write_ctl_word(stream, ModelCtl::Piggyback as u8)?;
+
+        write_ctl_word(stream, ModelCtl::Stream as u8)?;
+
+        let mut alg = [0 as u8; 1];
+        read_stream(stream, &mut alg)?;
+
+        let mut len_buf = [0 as u8; 8];
+        read_stream(stream, &mut len_buf)?;
+
+        let len = LittleEndian::read_u64(&mut len_buf);
+
+        let mut buf = vec![0 as u8; len as usize];
+        read_stream(stream, &mut buf)?;
+
+        Ok(FromPrimitive::from_u8(alg[0]))
     }
 }
